0c95af7c219fec548be3e138a02d2b625792ad60
신승빈 <binnie@172.20.nate.com>
2023-12-03 20:06:37 +0900

    demo2 logic edit

--- Holistic_turtleneck_demo2.py
+++ Holistic_turtleneck_demo2.py
@@ -15,22 +15,32 @@ save_directory = "static/images"
 if not os.path.exists(save_directory):
     os.makedirs(save_directory)
 
-# 거북목 감지 관련 변수
-NECK_THRESHOLD = 0.35
-captured = False
+# 캡쳐 간격 설정
+capture_interval = 10  # 10초마다 캡쳐
 
 # 이전 캡쳐 시간 초기화
 last_capture_time = datetime.datetime.now()
 
-def is_turtle_neck(landmarks):
-    # 거북목 감지
-    left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y
-    left_hip = landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y
-    right_shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y
-    right_hip = landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y
+def is_person_detected(landmarks):
+    # 사람 감지
+    return landmarks is not None
 
-    neck_length = max(left_shoulder, right_shoulder) - min(left_hip, right_hip)
-    return neck_length > NECK_THRESHOLD
+def cartoonize_image(img):
+    # 이미지를 회색조로 변환하고 블러 처리
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.medianBlur(gray, 7)
+
+    # 가장자리 검출
+    edges = cv2.adaptiveThreshold(gray_blur, 255,
+                                  cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, 9, 9)
+
+    # 컬러 이미지를 다운스케일링하여 색상을 단순화
+    color = cv2.bilateralFilter(img, 9, 300, 300)
+
+    # 만화 스타일로 이미지 병합
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    return cartoon
 
 while cap_side.isOpened():
     ret_side, frame_side = cap_side.read()
@@ -47,15 +57,20 @@ while cap_side.isOpened():
     image_side = cv2.cvtColor(image_side, cv2.COLOR_RGB2BGR)
 
     # 사람을 감지한 경우
-    if results_side.pose_landmarks and is_turtle_neck(results_side.pose_landmarks):
+    if is_person_detected(results_side.pose_landmarks):
         current_time = datetime.datetime.now()
         # 현재 시간과 이전 캡쳐 시간을 비교하여 10초마다 캡쳐
-        if (current_time - last_capture_time).total_seconds() >= 10:
+        if (current_time - last_capture_time).total_seconds() >= capture_interval:
+            # 골격 랜드마크 연결
+            mp_drawing = mp.solutions.drawing_utils
+            image_side_with_skeleton = frame_side.copy()
+            mp_drawing.draw_landmarks(image_side_with_skeleton, results_side.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
             # 이미지 저장
-            filename = current_time.strftime("image.jpg")
+            cartoon_image = cartoonize_image(image_side_with_skeleton)
+            filename = datetime.datetime.now().strftime("image.jpg")
             filepath = os.path.join(save_directory, filename)
-            cv2.imwrite(filepath, frame_side)
-            last_capture_time = current_time  # 캡쳐 시간 업데이트
+            cv2.imwrite(filepath, cartoon_image)
 
     cv2.imshow('Side Camera', image_side)
 
